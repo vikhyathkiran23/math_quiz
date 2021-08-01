5efdcb9267e5c5f22417ad69d230d35b0e0419f5
vikhyathkiran23 <72803191+vikhyathkiran23@users.noreply.github.com>
2021-08-01 19:46:48 +0530

    Delete quiz_game_login.js

--- quiz_game_login.js
+++
@@ -1,7 +0,0 @@
-var Player_1=document.getElementById("player_1_input_name").value;
-var Player_2=document.getElementById("player_2_input_name").value;
-function addUser(){
-    localStorage.setItem("Player_1", Player_1);
-    localStorage.setItem("Player_2", Player_2);
-    window.location="quiz_game_page.html";
-}
