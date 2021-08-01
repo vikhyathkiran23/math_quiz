b6b18634a6ae9457017b5f68b83af4b67531925d
vikhyathkiran23 <72803191+vikhyathkiran23@users.noreply.github.com>
2021-08-01 19:47:37 +0530

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,25 @@
+var Player_1=localStorage.getItem("Player_1");
+var Player_2=localStorage.getItem("Player_2");
+console.log(Player_1 + " " +Player_2)
+var Score_1=0;
+var Score_2=0;
+document.getElementById("Player_1_Name").innerHTML=Player_1 + ":";
+document.getElementById("Player_2_Name").innerHTML=Player_2 + ":";
+document.getElementById("Questioner").innerHTML=Player_1;
+document.getElementById("Answerer").innerHTML=Player_2;
+document.getElementById("player_1_score").innerHTML=Score_1;
+document.getElementById("player_2_score").innerHTML=Score_2;
+
+function send() {
+    var Number_1 = document.getElementById("Number_1").value;
+    var Number_2 = document.getElementById("Number_2").value;
+    Actual_Answer = parseInt(Number_1) * parseInt(Number_2);
+    Question_Number = "<h4 >" + Number_1 + " X " + Number_2 + "=" + "</h4><br>";
+    Input_Box = "Answer : <input type='number' id='input_check_box'>";
+    Check_Button= "<br><br><button class='btn btn-info' onclick='check()' >Check</button>";
+    row=Question_Number + Input_Box + Check_Button;
+    document.getElementById("output").innerHTML= row;
+    console.log("Hello");
+    document.getElementById("Number_1").value="";
+    document.getElementById("Number_2").value="";
+}
