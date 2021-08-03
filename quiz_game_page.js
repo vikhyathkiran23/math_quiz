187885acbcec0c555bb3976038cf0d09ae7150aa
vikhyathkiran23 <72803191+vikhyathkiran23@users.noreply.github.com>
2021-08-03 21:26:57 +0530

    Update quiz_game_page.js

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -3,6 +3,8 @@ var Player_2=localStorage.getItem("Player_2");
 console.log(Player_1 + " " +Player_2)
 var Score_1=0;
 var Score_2=0;
+var Questioner= "Player_1";
+var Answerer="Player_2"
 document.getElementById("Player_1_Name").innerHTML=Player_1 + ":";
 document.getElementById("Player_2_Name").innerHTML=Player_2 + ":";
 document.getElementById("Questioner").innerHTML=Player_1;
@@ -22,4 +24,35 @@ function send() {
     console.log("Hello");
     document.getElementById("Number_1").value="";
     document.getElementById("Number_2").value="";
+}
+
+function check(){
+    user_answer=document.getElementById("input_check_box").value;
+    if(user_answer==Actual_Answer){
+        if (Answerer=="Player_2") {
+            Score_2=Score_2+1;
+            document.getElementById("player_2_score").innerHTML=Score_2;         
+        }
+        else{
+            Score_1=Score_1+1;
+            document.getElementById("player_1_score").innerHTML=Score_1;
+        }
+    }
+    if(Questioner=="Player_1"){
+        Questioner="Player_2";
+        document.getElementById("Questioner").innerHTML=Player_2;
+    }
+    else{
+        Questioner="Player_1";
+        document.getElementById("Questioner").innerHTML= Player_1;
+    }
+    if(Answerer=="Player_1"){
+        Answerer="Player_2";
+        document.getElementById("Answerer").innerHTML= Player_2;
+    }
+    else{
+        Answerer="Player_1";
+        document.getElementById("Answerer").innerHTML=Player_1;
+    }
+    document.getElementById("output").innerHTML=""
 }
